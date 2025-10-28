8235b86719af15f220023c981bc4aaecb0b12f0f
walquer244 <walquervasconcelos@gmail.com>
2025-10-28 11:10:35 -0300

    Add files via upload

---
+++ BibliotecaPython/funcao.py
@@ -0,0 +1,12 @@
+
+listalivros=[]
+def adicionar_livros(listalivros):
+    return(input("digite o titulo do livro que procura: "))
+    return(input("me diga o autor do livro que voce procura: "))
+    return("disponivel")
+def emprestar_livro(listalivros):
+    return("livro não disponivel.")
+def devolver_livros(listalivros):
+    return("O livro está disponoivel novamente")
+def exibir_livros(listalivros):
+    return(listalivros)
