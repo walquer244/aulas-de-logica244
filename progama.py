73799d0f39f30a786f303a873e45328991c48d3c
walquer244 <walquervasconcelos@gmail.com>
2025-10-28 11:08:47 -0300

    biblioteca em python

---
+++ progama.py
@@ -0,0 +1,13 @@
+from funcao import *
+listalivros = []
+while True:
+    print("1 - Adicionar livro")
+    print("2 - Exibir todos os livros")
+    print("3 - Emprestar livro")
+    print("4 - Devolver livro")
+    print("0 - Sair")
+    op = input("digite a opcão desejada: ")
+
+    if op =="1":
+        adicionar_livros(())
+        listalivros.append(adicionar_livros(()))
