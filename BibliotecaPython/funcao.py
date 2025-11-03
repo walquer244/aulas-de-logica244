6efc41307b133cbcbf1b1160d5a6f0c5f0693308
walquer244 <isaac.sousa77@aluno.ce.gov.br>
2025-11-03 17:31:08 -0300

    bibliotecaPython

--- BibliotecaPython/funcao.py
+++ BibliotecaPython/funcao.py
@@ -1,12 +1,38 @@
 
-listalivros=[]
-def adicionar_livros(listalivros):
-    return(input("digite o titulo do livro que procura: "))
-    return(input("me diga o autor do livro que voce procura: "))
-    return("disponivel")
-def emprestar_livro(listalivros):
-    return("livro não disponivel.")
-def devolver_livros(listalivros):
-    return("O livro está disponoivel novamente")
-def exibir_livros(listalivros):
-    return(listalivros)
+def adicionar_livros(listaLivros):
+    titulo = input("Digite o titulo do seu livro: ")
+    autor = input("Digite o nome do autor do seu livro: ")
+    status = "Livro Disponivel"
+    dict = {
+        "titulo": titulo,
+        "autor" : autor,
+        "status" : status
+    }
+    listaLivros.append(dict)
+    return
+
+def emprestar_livros(listaLivros):
+    titulo = input("Digite o título do livro a ser emprestado: ")
+    for livro in listaLivros:
+        if livro["titulo"] == titulo:
+            livro["status"] = "Livro emprestado"
+            print(f'O livro "{titulo}" foi emprestado com sucesso.')
+            return
+    print("Livro não encontrado.")
+
+def devolver_livros(listaLivros):
+    titulo = input("Digite o título do livro a ser devolvido: ")
+    dia = input("Diga o dia em que o livro foi devolvido: ")
+    for livro in listaLivros:
+        if livro["titulo"] == titulo:
+            livro["status"] = f"Livro devolvido em {dia}"
+            print(f'O livro "{titulo}" foi devolvido com sucesso.')
+            return
+    print("Livro não encontrado.")
+
+def exibir_livros(listaLivros):
+    if not listaLivros:
+        print("Nenhum livro cadastrado.")
+    else:
+        for livro in listaLivros:
+            print(f'Título: {livro["titulo"]}, Autor: {livro["autor"]}, Status: {livro["status"]}')
--- BibliotecaPython/progama.py
+++ BibliotecaPython/progama.py
@@ -1,13 +1,29 @@
 from funcao import *
-listalivros = []
+
+listaLivros = []
+
 while True:
     print("1 - Adicionar livro")
-    print("2 - Exibir todos os livros")
-    print("3 - Emprestar livro")
-    print("4 - Devolver livro")
+    print("2 - Exibir todos os livros ")
+    print("4 - Devolver livro ")
     print("0 - Sair")
-    op = input("digite a opcão desejada: ")
+    op = input("Digite uma opção: ")
 
-    if op =="1":
-        adicionar_livros(())
-        listalivros.append(adicionar_livros(()))
+    if op == "1":
+        adicionar = adicionar_livros(listalivros)
+        listaLivros.append(dict)
+
+    elif op == "2":
+        exibir_livros(listaLivros)
+
+    elif op == "3":
+        emprestar_livro(listaLivros)
+
+    elif op == "4":
+        devolver_livros(listaLivros)
+
+    elif op == "0":
+        print("Obrigado por usar nossa biblioteca!!!")
+        break 
+    else:
+        print("Opção invalida!!")
